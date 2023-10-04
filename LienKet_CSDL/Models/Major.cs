574cf778f2688f9340e0c172e133db551133d83b
Nguyen The Quan <ntquan18102003@gmail.com>
2023-10-04 14:49:52 +0700

    Lab2

---
+++ LienKet_CSDL/Models/Major.cs
@@ -0,0 +1,9 @@
+﻿namespace LienKet_CSDL.Models
+{
+    public class Major
+    {
+        public int MajorID { get; set; }
+        public string MajorName { get; set; }
+        public ICollection<Learner> Learners { get; set; }
+    }
+}
